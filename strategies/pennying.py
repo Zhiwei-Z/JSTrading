40642455334b9855a285e700098bf7ff54249c29
Zhiwei-Z <zhiweizhang2012@gmail.com>
2019-02-09 13:05:44 -0800

    fixing pennying

--- strategies/pennying.py
+++ strategies/pennying.py
@@ -9,29 +9,29 @@ def trade(exchange):
     if GSb:
         best_bid = highest_buy(GSb)
         best_offer = lowest_sell(GSb)
-        # if best_bid[0] - 1 > best_offer[0] + 1:
-        #     trades.append(('BUY', 'GS', best_offer[0] + 1, best_offer[1]))
-        #     trades.append(('SELL', 'GS', best_bid[0] - 1, best_bid[1]))
-        trades.append(('BUY', 'GS', best_offer[0] + 1, best_offer[1]))
-        trades.append(('SELL', 'GS', best_bid[0] - 1, best_bid[1]))
+        if best_bid[0] + 1 < best_offer[0] - 1:
+            trades.append(('SELL', 'GS', best_offer[0] - 1, best_offer[1]))
+            trades.append(('BUY', 'GS', best_bid[0] + 1, best_bid[1]))
+        # trades.append(('BUY', 'GS', best_offer[0] + 1, best_offer[1]))
+        # trades.append(('SELL', 'GS', best_bid[0] - 1, best_bid[1]))
 
     if MSb:
         best_bid = highest_buy(MSb)
         best_offer = lowest_sell(MSb)
-        # if best_bid[0] - 1 > best_offer[0] + 1:
-        #     trades.append(('BUY', 'MS', best_offer[0] + 1, best_offer[1]))
-        #     trades.append(('SELL', 'MS', best_bid[0] - 1, best_bid[1]))
-        trades.append(('BUY', 'MS', best_offer[0] + 1, best_offer[1]))
-        trades.append(('SELL', 'MS', best_bid[0] - 1, best_bid[1]))
+        if best_bid[0] + 1 < best_offer[0] - 1:
+            trades.append(('SELL', 'MS', best_offer[0] - 1, best_offer[1]))
+            trades.append(('BUY', 'MS', best_bid[0] + 1, best_bid[1]))
+        # trades.append(('BUY', 'MS', best_offer[0] + 1, best_offer[1]))
+        # trades.append(('SELL', 'MS', best_bid[0] - 1, best_bid[1]))
 
     if WFCb:
         best_bid = highest_buy(WFCb)
         best_offer = lowest_sell(WFCb)
-        # if best_bid[0] - 1 > best_offer[0] + 1:
-        #     trades.append(('BUY', 'WFC', best_offer[0] + 1, best_offer[1]))
-        #     trades.append(('SELL', 'WFC', best_bid[0] - 1, best_bid[1]))
-        trades.append(('BUY', 'WFC', best_offer[0] + 1, best_offer[1]))
-        trades.append(('SELL', 'WFC', best_bid[0] - 1, best_bid[1]))
+        if best_bid[0] + 1 < best_offer[0] - 1:
+            trades.append(('SELL', 'WFC', best_offer[0] - 1, best_offer[1]))
+            trades.append(('BUY', 'WFC', best_bid[0] + 1, best_bid[1]))
+        # trades.append(('BUY', 'WFC', best_offer[0] + 1, best_offer[1]))
+        # trades.append(('SELL', 'WFC', best_bid[0] - 1, best_bid[1]))
 
     return trades
 
