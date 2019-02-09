2af5fbcb627212d0569b021f69555cc6943cb368
yuhongc <mikechen212@gmail.com>
2019-02-09 12:40:16 -0800

    prettify

--- strategies/print_latest_books.py
+++ strategies/print_latest_books.py
@@ -1,8 +1,9 @@
 import numpy as np
+import pprint as pp
 
 
 def trade(exchange):
     books = exchange.latest_books
     if np.random.randint(1, 100) == 1:
-        print(books)
+        pp.pprint(books)
     return []
