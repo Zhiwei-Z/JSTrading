85743571301f438b51d7be762405348116d02ae1
yuhongc <mikechen212@gmail.com>
2019-02-09 12:17:31 -0800

    sda

--- strategies/pennying.py
+++ strategies/pennying.py
@@ -4,13 +4,33 @@ def trade(exchange):
     '''pennnying strategy is to always buy at best_bid + 1, sell at best_offer - 1.'''
     trades = []
     if data['type'] == 'book' and data['symbol'] == "GS":
-        bids = data['buy']
-        if bids != []:
-            best_bid = bids[0]
-        offers = data['sell']
-        if offers != []:
-            best_offer = offers[0]
+        best_bid = highest_buy(data)
+        best_offer = lowest_sell(data)
         if best_bid > best_offer:
             trades.append(('SELL', data['symbol'], best_bid[0] + 1, 10))
             trades.append(('SELL', data['symbol'], best_offer[0] - 1, 10))
     return trades
+
+
+def lowest_sell(book):
+    asks = book['sell']
+    low = 100000000
+    s = 0
+    for price, size in asks:
+        low = min(low, price)
+        s = size
+    return low, s
+
+
+def highest_buy(book):
+    bids = book['buy']
+    h = 0
+    s = 0
+    for price, size in bids:
+        h = max(h, price)
+        s = size
+    return h, s
+
+
+def fair_price(book):
+    return (lowest_sell(book)[0] + highest_buy(book)[0]) / 2
