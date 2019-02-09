20c78813035d0e98d947812684e781d1f281050d
yuhongc <mikechen212@gmail.com>
2019-02-09 14:48:35 -0800

    add cancelling

--- Connection.py
+++ Connection.py
@@ -67,8 +67,10 @@ class ExchangeConnection:
         trade = {'type': 'add', 'order_id': self.order_id, 'symbol': symbol,
                  'dir': buysell, 'price': price, 'size': size}
         self.order_id += 1
+        """
         if self.order_id>5000:
             self.cancel(self.order_id-5000)
+        """
         # print(trade)
         self.write(trade)
 
